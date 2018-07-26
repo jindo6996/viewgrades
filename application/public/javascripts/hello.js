50f085a6ea742f8930b2c606e6fdacced85925bd
Nguyen Tai Dai <nguyen_td@septeni-technology.jp>
2018-07-26 15:26:45 +0700

    fix comment

--- application/public/javascripts/hello.js
+++ application/public/javascripts/hello.js
@@ -1,11 +1,6 @@
 $(document).ready(function () {
-    if (window.console) {
-        console.log("Welcome to your Play application's JavaScript!");
-    }
-
     $(function(){
         $('#email').focusin(function(){
-          console.log("sjhdfksdf");
             $('dd.error').hide();
             $('span.error').hide();
         });
