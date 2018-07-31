0e0b4c133308f565d3b9efd8e424045241eb1cde
Nguyen Tai Dai <nguyen_td@septeni-technology.jp>
2018-07-31 14:14:57 +0700

    update

--- application/test/controllers/AuthControllerSpec.scala
+++ application/test/controllers/AuthControllerSpec.scala
@@ -9,11 +9,11 @@ import play.api.test.CSRFTokenHelper._
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 
-import scala.util.Failure
+import scala.util.{ Failure, Success }
 
 class AuthControllerSpec extends Specification with Mockito {
-  val mockUserDao: UserRepository = mock[UserRepository]
-  val controller = new AuthController(mockUserDao, stubControllerComponents())
+  val mockUserRepo: UserRepository = mock[UserRepository]
+  val controller = new AuthController(mockUserRepo, stubControllerComponents())
   "AuthController" >> {
     "loginView" >> {
       "go to page login if not logged in" >> {
@@ -30,53 +30,40 @@ class AuthControllerSpec extends Specification with Mockito {
         val result = controller.processLogin().apply(FakeRequest(POST, "/").
           withFormUrlEncodedBody("email" -> "", "password" -> "123456").withCSRFToken)
         status(result) must equalTo(303)
-        contentAsString(result) must contain("error")
       }
 
       "password is empty" >> {
         val result = controller.processLogin().apply(FakeRequest(POST, "/").
           withFormUrlEncodedBody("email" -> "test@gmail.com", "password" -> "").withCSRFToken)
         status(result) must equalTo(303)
-        contentAsString(result) must contain("error")
-      }
-
-      "email format is incorrect" >> {
-        val result = controller.processLogin().apply(FakeRequest(POST, "/").
-          withFormUrlEncodedBody("email" -> "test", "password" -> "123456").withCSRFToken)
-        status(result) must equalTo(303)
-        contentAsString(result) must contain("error.email")
       }
 
       "email incorrect or user not exists" >> {
-        mockUserDao.resolveByEmail("test@gmail.com") returns Failure(EmailNotFoundException("Email not found"))
+        mockUserRepo.resolveByEmail("test@gmail.com") returns Failure(EmailNotFoundException("Email not found"))
         val result = controller.processLogin().apply(FakeRequest(POST, "/").
           withFormUrlEncodedBody("email" -> "test@gmail.com", "password" -> "123456").withCSRFToken)
         status(result) must equalTo(303)
-        contentAsString(result) must contain("User not found")
       }
 
       "user exists but password incorrect" >> {
-        //        val user = User(UserId("1"), "test@gmail.com", "123123", "2013/1/1", UserRole("STAFF"), Department("BO"), 12, UserStatus("ACTIVE"))
-        //        mockUserDao.resolveByEmail("test@gmail.com") returns Success(user)
-        //        mockUserDao.checkPassword("123455", user.password) returns false
-        //        val result = controller.processLogin.apply(FakeRequest(POST, "/").
-        //          withFormUrlEncodedBody("email" -> "test@gmail.com", "password" -> "123455").withCSRFToken)
-        //        status(result) must equalTo(BAD_REQUEST)
-        //        contentAsString(result) must contain("User not found")
-        1 mustEqual (1)
+        val user = User(UserId("1"), "test@gmail.com", "123123", "2013/1/1", UserRole.Staff, Department("BO"), 12, UserStatus.Active)
+        mockUserRepo.resolveByEmail("test@gmail.com") returns Success(user)
+        mockUserRepo.checkPassword("123455", user.password) returns false
+        val result = controller.processLogin.apply(FakeRequest(POST, "/").
+          withFormUrlEncodedBody("email" -> "test@gmail.com", "password" -> "123455").withCSRFToken)
+        status(result) must equalTo(303)
       }
 
       "user login successfully" >> {
-        //        val user = User(UserId("1"), "test@gmail.com", "123123", "2013/1/1", UserRole("STAFF"), Department("BO"), 12, UserStatus("ACTIVE"))
-        //        mockUserDao.resolveByEmail("test@gmail.com") returns Success(user)
-        //        mockUserDao.checkPassword("123456", user.password) returns true
-        //        val result = controller.processLogin().apply(FakeRequest(POST, "/").
-        //          withFormUrlEncodedBody("email" -> "test@gmail.com", "password" -> "123456").withCSRFToken)
-        //        status(result) must equalTo(303)
-        1 mustEqual (1)
+        val user = User(UserId("1"), "test@gmail.com", "123123", "2013/1/1", UserRole.Staff, Department("BO"), 12, UserStatus.Active)
+        mockUserRepo.resolveByEmail("test@gmail.com") returns Success(user)
+        mockUserRepo.checkPassword("123456", user.password) returns true
+        val result = controller.processLogin().apply(FakeRequest(POST, "/").
+          withFormUrlEncodedBody("email" -> "test@gmail.com", "password" -> "123456").withCSRFToken)
+        status(result) must equalTo(303)
       }
       "System error" >> {
-        mockUserDao.resolveByEmail("test@gmail.com") returns Failure(new Exception("System error"))
+        mockUserRepo.resolveByEmail("test@gmail.com") returns Failure(new Exception("System error"))
         controller.processLogin.apply(FakeRequest(POST, "/").
           withFormUrlEncodedBody("email" -> "test@gmail.com", "password" -> "123455").withCSRFToken) must throwA[Exception]
       }
