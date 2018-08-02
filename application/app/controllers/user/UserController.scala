bce5bb5b9ae16e1f0bb3baf55619a531f5fa6853
Nguyen Viet Tu <tu_nv@septeni-technology.jp>
2018-08-02 14:54:03 +0700

    add secure pass and imple custom pagging

--- application/app/controllers/user/UserController.scala
+++ application/app/controllers/user/UserController.scala
@@ -6,20 +6,38 @@ import model._
 import play.api.mvc.{ AbstractController, ControllerComponents }
 import controllers.forms.AddUserForm._
 import controllers.forms.EditUserForm._
+import org.apache.commons.lang3.RandomStringUtils
 
 @Singleton
 class UserController @Inject() (userRepository: UserRepository, cc: ControllerComponents) extends AbstractController(cc)
   with play.api.i18n.I18nSupport with controllers.BaseController with Secured {
+
+  def randomString: String = {
+    val upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    val lowerChar = "abcdefghijklmnopqrstuvwxyz"
+    val number = "0123456789"
+    val specialChar = "!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+
+    val start = 8
+    val end = 20
+    val rnd = new scala.util.Random
+    val randomLenght = start + rnd.nextInt((end - start) + 1)
+
+    val pwd = RandomStringUtils.random(randomLenght, upperChar + lowerChar + number + specialChar)
+    if (pwd.exists(upperChar.contains(_)) && pwd.exists(lowerChar.contains(_)) && pwd.exists(number.contains(_)) && pwd.exists(specialChar.contains(_)))
+      pwd
+    else
+      randomString
+  }
+
   def listUser = withAuth { email => implicit request =>
-
-    Ok(views.html.users.userlist(userRepository.resolveAll.get, addUserForm, editUserForm))
-
+    Ok(views.html.users.userlist(userRepository.resolveAll.get, addUserForm, editUserForm, randomString))
   }
 
   def processAddUser = withAuth { email => implicit request =>
     (for {
       userInfo <- validateForm(addUserForm)
-      userInfo <- userRepository.store(User(UserId(userInfo.userId), userInfo.email, "123456", userInfo.entryCompanyDate, UserRole.fromString(userInfo.userRole).get, Department(userInfo.department), userInfo.annualLeave, UserStatus.Active, ""))
+      addToDB <- userRepository.store(User(UserId(userInfo.userId), userInfo.email, "123456", userInfo.entryCompanyDate, UserRole.fromString(userInfo.userRole).get, Department(userInfo.department), userInfo.annualLeave, UserStatus.Active, ""))
     } yield {
       Redirect("/users")
     }).recover {
