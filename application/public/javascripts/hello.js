aaa780f5de6f1faed3a23ee8afac5186f60890e7
Nguyen Tai Dai <nguyen_td@septeni-technology.jp>
2018-08-03 18:24:08 +0700

    Merge branch 'UserStory2' of https://gitlab.devsep.com/nguyen_td/tsm into UserStory2
    # Conflicts:
    #	application/app/views/main.scala.html

--- application/public/javascripts/hello.js
+++ application/public/javascripts/hello.js
@@ -1,5 +1,5 @@
-$(document).ready(function () {
     $(function(){
+        console.log("dadad");
         $('#email').focusin(function(){
             $('dd.error').hide();
             $('span.error').hide();
@@ -12,22 +12,22 @@ $(document).ready(function () {
             $('dd.error').hide();
             $('span.error').hide();
         });
-        $('.editUser').click(function(){
-            var row = $(this).closest("tr");
-            $("#userIdEdit").val(row.find("td:nth-child(1)").text());
-            $("#emailEdit").val(row.find("td:nth-child(2)").text());
-            //4:ngay nghi phep, 3departmen, 5date entry, 6role,7 status, 8 last
-            $("#entryCompanyDateEdit").val(row.find("td:nth-child(5)").text());
-            $("#annualLeaveEdit").val(row.find("td:nth-child(4)").text());
-            var role="#userRoleEdit_"+ row.find("td:nth-child(6)").text().toUpperCase();
-            $(role).prop("checked", true);
-            var status="#statusEdit_"+ row.find("td:nth-child(7)").text().toUpperCase();
-            $(status).prop("checked", true);
-            $("#departmentEdit").val(row.find("td:nth-child(3)").text());
-
-        });
     });
 
-});
-
-
+$('.editUser').click(
+    function getData() {
+        console.log(this);
+        var row = $(this).closest("tr");
+        alert(row.length);
+        $("#userIdEdit").val(row.find("td:nth-child(1)").text());
+        $("#emailEdit").val(row.find("td:nth-child(2)").text());
+        //4:ngay nghi phep, 3departmen, 5date entry, 6role,7 status, 8 last
+        $("#entryCompanyDateEdit").val(row.find("td:nth-child(5)").text());
+        $("#annualLeaveEdit").val(row.find("td:nth-child(4)").text());
+        var role = "#userRoleEdit_" + row.find("td:nth-child(6)").text().toUpperCase();
+        $(role).prop("checked", true);
+        var status = "#statusEdit_" + row.find("td:nth-child(7)").text().toUpperCase();
+        $(status).prop("checked", true);
+        $("#departmentEdit").val(row.find("td:nth-child(3)").text());
+    }
+);
