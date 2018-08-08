22845379ece44308fe5df4be52a89fb38769d7db
Nguyen Tai Dai <nguyen_td@septeni-technology.jp>
2018-08-08 17:26:07 +0700

    update

--- application/app/controllers/grade/GradeController.scala
+++ application/app/controllers/grade/GradeController.scala
@@ -7,9 +7,7 @@ import play.api.mvc.{ AbstractController, ControllerComponents }
 
 class GradeController @Inject() (gradeRepository: GradeRepository, cc: ControllerComponents) extends AbstractController(cc)
   with play.api.i18n.I18nSupport with controllers.BaseController with Secured with Authentication {
-  def yourGrade = Action { implicit request =>
-    //    val re = gradeRepository.resolveAllByEmail(getMailInSession.get).get
-    //    print(getMailInSession.get)
+  def yourGrade = withAuth { email => implicit request =>
     Ok(views.html.grades.yourgrade(gradeRepository.resolveAllByEmail(getMailInSession.get).get))
   }
 
